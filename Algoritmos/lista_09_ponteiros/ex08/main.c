fb750176030a996312b047298a686cf282057429
André Felipe <andrefwalves@gmail.com>
2024-12-08 15:19:05 -0300

    create ex 08

---
+++ Algoritmos/lista_09_ponteiros/ex08/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int get_hamming_distance(const char *str1, const char *str2)
+{
+    int hamming = 0;
+
+    while (*str1 != '\0' && *str2 != '\0')
+    {
+        if (*str1 != *str2)
+        {
+            hamming++;
+        }
+        str1++;
+        str2++;
+    }
+    return hamming;
+}
+
+int main()
+{
+    int dist = get_hamming_distance("banana", "cabana");
+    printf("Hamming: %d\n", dist);
+    return 0;
+}
