d886f2738f79c93f74f7574917e25adbcf4dfd0c
andre-felipe-wonsik-alves <aalves.2020@alunos.utfpr.edu.br>
2025-05-23 17:26:58 -0300

    aula alg

--- 3° Período/Algoritmos e ED1/Trabalho_busca_e_ordenacao/codigo/gerador_dados.cpp
+++ 3° Período/Algoritmos e ED1/Trabalho_busca_e_ordenacao/codigo/gerador_dados.cpp
@@ -2,15 +2,52 @@
 #include <string>
 #include <vector>
 #include <list>
+#include <fstream>
+#include <random>
 
-void create_file(const char *filepath, int size)
+using namespace std;
+void create_ordered_file(int file_size)
 {
-    FILE *file = fopen(filepath, "w");
-    std::cout << "teste";
-    fwrite("asd", sizeof(char), size, file);
+    fstream file;
+    file.open("./dados/ordenado.txt", ios::binary | ios::out);
+
+    int random = rand() % 9999;
+    int before = random;
+
+    for (int i = file_size; i >= 0; i--)
+    {
+        if (random >= before)
+        {
+            file << random;
+            file << ".";
+        }
+        before = random;
+        random = rand() % 9999;
+    }
+
+    file.close();
+}
+
+void create_unordered_file(int file_size)
+{
+    fstream file;
+    file.open("./dados/nao_ordenado.txt", ios::binary | ios::out);
+
+    int random = rand() % 9999;
+
+    for (int i = file_size; i >= 0; i--)
+    {
+        file << random;
+        random = rand() % 9999;
+    }
+
+    file.close();
 }
 
 int main()
 {
-    create_file("./", 30);
+    // create_unordered_file(100);
+    create_ordered_file(100);
+
+    return 0;
 }
