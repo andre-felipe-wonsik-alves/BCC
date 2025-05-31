00f82789e8e7c8c7a5dfbd1a9c39292a46758717
andre-felipe-wonsik-alves <aalves.2020@alunos.utfpr.edu.br>
2025-05-30 22:51:24 -0300

    insertion sort e main

--- 3° Período/Algoritmos e ED1/Trabalho_busca_e_ordenacao/codigo/ordenacao.cpp
+++ 3° Período/Algoritmos e ED1/Trabalho_busca_e_ordenacao/codigo/ordenacao.cpp
@@ -8,30 +8,64 @@
 
 using namespace std;
 
-class Ordenacao {
+class Ordenacao
+{
+private:
     vector<int> dados;
 
-    Ordenacao(vector<int> &_arr){
+    Ordenacao(vector<int> &_arr)
+    {
         this->dados = _arr;
     }
 
-    void selection_sort(){
+    void selection_sort()
+    {
         vector<int> copia = this->dados;
-        int minimum_index = copia[0];
-        int tamanho = copia.size()
+        int tamanho = copia.size();
+        int index_minimo;
 
-        for(int step = 0; step < size - 1; step++) {
-            int min_idx = step;
-            for (int i = step + 1; i < size; i++) {
+        for (int i = 0; i < tamanho; i++)
+        {
+            int index_minimo = i;
 
-            // To sort in descending order, change > to < in this line.
-            // Select the minimum element in each loop.
-            if (array[i] < array[min_idx])
-                min_idx = i;
+            for (int j = i + 1; j < tamanho; j++)
+            {
+                if (copia[j] < copia[index_minimo])
+                    index_minimo = j;
             }
 
-            // put min at the correct position
-            swap(&array[min_idx], &array[step]);
+            this->swap(&copia[index_minimo], &copia[i]);
         }
+    }
+
+    void insertion_sort()
+    {
+        vector<int> copia = this->dados;
+        int tamanho = copia.size();
+
+        for (int i = 1; i < tamanho; i++) // consideramos que o primeiro elemento esta ordenado
+        {
+            int temp = copia[i];
+            int j = i--;
+
+            while (j >= 0 && temp < copia[j])
+            {
+                copia[j + 1] = copia[j];
+                --j;
+            }
+
+            copia[j + 1] = temp;
+        }
+    }
+
+    void bubble_sort()
+    {
+    }
+
+    void swap(int *elem_A, int *elem_B)
+    {
+        int temp = *elem_A;
+        *elem_A = *elem_B;
+        *elem_B = temp;
     }
 };
